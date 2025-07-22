aaff2ba619517cccd6481813e6f8c58bc4410a22
Heru Handika <herubiolog@gmail.com>
2025-07-22 15:36:30 -0400

    Add basic data structure.

--- backend/app/database/lance.py
+++ backend/app/database/lance.py
@@ -1,2 +1,15 @@
+from lancedb.pydantic import LanceModel, Vector
+from lancedb.embeddings import get_registry
+
 DB_DIR = "lance_db"
 DB_FILE = "biocosmos.lance"
+
+clip = get_registry().get("open-clip").create()
+
+
+class DbSchema(LanceModel):
+    img_id: str
+    species: str
+    # img_uri: str = clip.SourceField()
+    img_bytes: bytes = clip.SourceField()
+    clip_embeddings: Vector(clip.ndims()) = clip.VectorField()
