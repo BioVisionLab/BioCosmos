cc849bda2b9b748223fede3abe3e87d012d5c1d1
Heru Handika <herubiolog@gmail.com>
2025-09-09 13:05:16 -0400

    Clean styles.

--- src/app/species/[speciesName]/components/similar_species.tsx
+++ src/app/species/[speciesName]/components/similar_species.tsx
@@ -13,12 +13,15 @@ function SimilarSpecies({
     return null; // Don't render anything if there are no similar species
   }
   return (
-    <div className="mt-4">
-      <h2 className="text-2xl font-semibold">Visually Similar Species</h2>
-      <p className="text-gray-500 dark:text-gray-400">
-        Similar species to <span className="italic">{species}</span> based on
-        image analysis.
-      </p>
+    <div className="mt-4 border border-gray-300 dark:border-gray-600 rounded-xl bg-white/70 dark:bg-gray-800/70 backdrop-blur shadow">
+      <div className="border-b border-gray-300 dark:border-gray-600 p-4">
+        <h2 className="text-2xl font-semibold">Visually Similar Species</h2>
+        <p className="text-gray-500 dark:text-gray-400">
+          Similar species to <span className="italic">{species}</span> based on
+          image analysis.
+        </p>
+      </div>
+
       <SimilarSpeciesImageGallery imgIds={imgIds} />
     </div>
   );
@@ -26,7 +29,7 @@ function SimilarSpecies({
 
 function SimilarSpeciesImageGallery({ imgIds }: { imgIds: string[] }) {
   return (
-    <div className="mt-2 p-2 border border-gray-300 dark:border-gray-600 overflow-x-auto rounded-xl">
+    <div className="overflow-x-auto rounded-xl px-2">
       {imgIds.map((imgId) => (
         <SimilarSpeciesImage key={imgId} imgId={imgId} />
       ))}
@@ -51,7 +54,7 @@ function SimilarSpeciesImage({ imgId }: { imgId: string }) {
 
   return (
     imageUrl && (
-      <div className="relative w-32 h-32 inline-block m-2 overflow-hidden p-2">
+      <div className="relative w-32 h-32 inline-block m-2 overflow-hidden">
         <Image
           src={imageUrl}
           alt={`Similar species image ${imgId}`}
