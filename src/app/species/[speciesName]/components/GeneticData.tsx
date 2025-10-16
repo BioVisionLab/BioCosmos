08a8f884979e6fd8d401d13a70a344a3a38dcfca
Heru Handika <herubiolog@gmail.com>
2025-10-16 16:04:59 -0400

    Update labels.

--- src/app/species/[speciesName]/components/GeneticData.tsx
+++ src/app/species/[speciesName]/components/GeneticData.tsx
@@ -59,7 +59,7 @@ export function GeneticData({ speciesName }: GeneticPageProps) {
       )}
       {geneCounts && (
         <div>
-          <h3 className="text-lg">Known gene counts</h3>
+          <h3 className="text-lg">Sequenced genes</h3>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {Object.entries(geneCounts).map(([type, count]) => (
               <GeneCounts key={type} geneType={type} count={count} />
