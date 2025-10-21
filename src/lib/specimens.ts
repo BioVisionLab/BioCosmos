3061a131cbf8a2181101a6ab7add494ccf2880c7
Heru Handika <herubiolog@gmail.com>
2025-10-21 14:15:37 -0400

    Ignore requests.

---
+++ src/lib/specimens.ts
@@ -0,0 +1,15 @@
+// function getSpecimenStats(speciesName: string) {
+//   const response = fetch(
+//     `api/specimens/stats?species=${encodeURIComponent(speciesName)}`
+//   );
+
+//   if (!response.ok) {
+//     throw new Error(
+//       `Failed to fetch specimen stats. Status: ${response.status}`
+//     );
+//   }
+
+//   return response.json();
+// }
+
+// export { getSpecimenStats };
